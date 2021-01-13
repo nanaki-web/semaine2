965eaa0912057d00ca5fcfd864633e0026eee08a
Anne Wilga <anne.wilga@orange.fr>
2021-01-13 17:06:21 +0100

    interroger 6a10

--- interroger_bdd/interroger.sql
+++ interroger_bdd/interroger.sql
@@ -32,15 +32,73 @@ WHERE cus_city LIKE '%divos%'
 
 -- Q6. Quel est le produit vendu le moins cher ? Afficher le prix, l'id et le nom du produit.
 
-SELECT pro_id,pro_name,pro_price
+SELECT pro_name,pro_id,pro_price
 FROM products
-ORDER BY pro_price ASC
-LIMIT 1
+WHERE pro_price=(SELECT MIN(pro_price)
+FROM products)
 
 -- Q7. Lister les produits qui n'ont jamais été vendus
 
-select pro_id,pro_ref,pro_name
+SELECT pro_id,pro_name,pro_ref
 FROM products
-WHERE pro_id BETWEEN 34 AND 42 
+where pro_id NOT IN 
+(SELECT ode_pro_id
+FROM orders_details)
+
+
+
+
+-- Q8. Afficher les produits commandés par Madame Pikatchien.
+
+SELECT pro_id,pro_ref,pro_name,cus_id,ord_id,ode_id
+FROM products,customers,orders_details,orders
+where cus_id = ord_cus_id AND cus_lastname="Pikatchien" and ord_id=ode_ord_id and ode_pro_id=pro_id
+
+
+
+SELECT pro_id,pro_ref,pro_name,cus_id,ord_id,ode_id
+FROM products
+JOIN orders_details
+ON products.pro_id = orders_details.ode_pro_id
+JOIN orders
+ON orders_details.ode_ord_id = orders.ord_id
+JOIN customers
+ON orders.ord_cus_id = customers.cus_id
+WHERE cus_lastname ='Pikatchien'
+
+
+
+-- Q9. Afficher le catalogue des produits par catégorie, le nom des produits et de la catégorie doivent être affichés.
+
+SELECT cat_id, cat_name,pro_name 
+FROM categories 
+INNER JOIN products 
+ON categories.cat_id = products.pro_cat_id 
+ORDER BY cat_name ASC
+
+
+SELECT cat_id, cat_name,pro_name 
+FROM categories ,products
+WHERE cat_id = pro_cat_id
+ORDER BY cat_name ASC
+
+-- Q10. Afficher l'organigramme hiérarchique (nom et prénom et poste des employés) du magasin de Compiègne, classer par ordre alphabétique. Afficher le nom et prénom des employés, éventuellement le poste (si vous y parvenez).
+
+SELECT emp_superior_id,emp_lastname,emp_firstname
+FROM employees
+
+SELECT emp_lastname,emp_firstname
+FROM employees
+
+SELECT concat (emp_lastname,' ',emp_firstname)as 'employé' , concat (emp_superior_id,' ',emp_lastname,' ',emp_firstname)AS 'supérieur'
+FROM employees
+
+-- Fonctions d'agrégation
+-- Q11. Quel produit a été vendu avec la remise la plus élevée ? Afficher le montant de la remise, le numéro et le nom du produit, le numéro de commande et de ligne de commande.
+
+SELECT MAX(ode_discount),ode_pro_id,pro_name,ord_id
+FROM orders_details,products,orders
+
+
 
 
